83b701775a278834904ac54d62826d10953a66eb
githubmor <pc2man@gmail.com>
2023-09-29 10:26:31 +0330

    add curve background

---
+++ app/components/icons/curves/main-curve/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type Props = {
+    
+}
+
+export default function MainCurve({}: Props) {
+  return (
+    <svg
+      width="978"
+      height="1024"
+      viewBox="0 0 978 1024"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M599.322 99.8342C503.965 29.2924 356.089 2.79909 273.874 0H-3V1024H838.29C888.779 1003.78 989.279 840.252 976.964 707.703C963.706 565.014 836.264 543.618 753.858 402.602C695.155 302.147 703.702 177.051 599.322 99.8342Z"
+        fill="url(#paint0_linear_281_1981)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_281_1981"
+          x1="400.988"
+          y1="726.773"
+          x2="767.203"
+          y2="-435.6"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#C9E8FE" />
+          <stop offset="1" stop-color="#BCF1FF" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
